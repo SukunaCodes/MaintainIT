f51cc35f8865f1886a1506d195d1a9db4e9af378
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2023-07-06 20:08:17 +0300

    Status: Added models and restructured the routes.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -9,14 +9,27 @@
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
                     <li class=active><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
                 </ul>
             </li>
-            <li class="menu-header">Starter</li>
-
+            <li class="dropdown active">
+                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Agent Suppliers</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="index-0.html">Add Agent Supplier</a></li>
+                    <li class=active><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Log Out</li>
+            <li>
+                <a href="{{route('logout')}}" class="nav-link" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();"><i class="fas fa-fire"></i><span>Log Out</span></a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
 
         </ul>
     </aside>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return view('index');
+});*/
 
 Auth::routes();
 
 Route::middleware(['auth']) -> group(function(){
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 });
